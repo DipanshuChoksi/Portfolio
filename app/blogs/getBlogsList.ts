1e5731c949538f3f6ea8f80cf45acacd88b9da8d
Dipanshu Choksi <kbdipanshuchoksi001088@gmail.com>
2025-11-11 03:02:10 +0530

    fix: add error handling in getBlogsList and fetchPost functions

--- app/blogs/getBlogsList.ts
+++ app/blogs/getBlogsList.ts
@@ -1,11 +1,12 @@
 import { BlogPage, BlogNode } from "@/interfaces";
 
 export async function getBlogsList(name: string): Promise<BlogNode[]> {
-  const stream = await fetch("https://gql.hashnode.com", {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify({
-      query: `query Publication {
+  try {
+    const stream = await fetch("https://gql.hashnode.com", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({
+        query: `query Publication {
                 publication(host: "${name}.hashnode.dev") {
                     isTeam
                     title
@@ -34,11 +35,15 @@ export async function getBlogsList(name: string): Promise<BlogNode[]> {
                     }
                 }
             }`,
-    }),
-  });
-  const data = await stream.json();
-  let nodeList: BlogNode[] = data?.data?.publication?.posts?.edges ?? [];
-  return nodeList;
+      }),
+    });
+    const data = await stream.json();
+    let nodeList: BlogNode[] = data?.data?.publication?.posts?.edges;
+    if (nodeList) return nodeList;
+  } catch (error) {
+    console.error;
+  }
+  return [];
 }
 
 export async function fetchPost({
@@ -47,7 +52,7 @@ export async function fetchPost({
 }: {
   host: string;
   slug: string;
-}): Promise<BlogPage> {
+}): Promise<BlogPage | null> {
   const query = `
     query SinglePost($host: String!, $slug: String!) {
       publication(host: $host) {
@@ -71,13 +76,18 @@ export async function fetchPost({
   `;
   const variables = { host: host + ".hashnode.dev", slug };
 
-  const response = await fetch("https://gql.hashnode.com", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query, variables }),
-  });
-  const json = await response.json();
-  return json.data.publication.post;
+  try {
+    const response = await fetch("https://gql.hashnode.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query, variables }),
+    });
+    const json = await response.json();
+    return json?.data?.publication?.post;
+  } catch (error) {
+    console.error;
+  }
+  return null;
 }
